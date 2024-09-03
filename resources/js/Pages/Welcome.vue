319f0222a81a46640bc0e1963aefc4c458c08b18
Ambrose <ambkyusya@gmail.com>
2024-09-03 13:27:45 +0300

    Generate API docs

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -183,12 +183,14 @@ export default {
                                 Log in
                             </Link>
 
-                            <Link
-                                v-if="canRegister"
+                            <a
                                 href="https://bookings.test/docs/index.html"
+                                target="_blank"
+                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                             >
-                                <Button v-if="canRegister" type="success" shape="circle">Sign Up</Button>
-                            </Link>
+
+                                Documentation
+                            </a>
 
                         </template>
                     </nav>
